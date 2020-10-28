5b12de5eeaf041f9537adf4a4f29782dd8110a82
Jonathan Ho <hojon389@gmail.com>
2020-10-28 15:55:23 -0400

    withdraw works

--- BankAccount.java
+++ BankAccount.java
@@ -8,13 +8,12 @@ public class BankAccount{
     password = pass;
   }
   public double getBal(){
+    System.out.print("Your balance is ");
     return balance;
   }
   public int getID(){
+    System.out.print("Your ID is ");
     return accountID;
-  }
-  public String getPass(){
-    return password;
   }
   public void setPass(String password1){
     password = password1;
@@ -27,5 +26,17 @@ public class BankAccount{
     else{
       return false;
     }
+  }
+  public boolean withdraw(double amount){
+    if(amount < 0 || balance < amount){
+      return false;
+    }
+    else{
+      balance -= amount;
+      return true;
+    }
+  }
+  public String toString(){
+    return "ID:" + accountID + " "+ "Balance:" + balance;
   }
 }
--- BankTester.java
+++ BankTester.java
@@ -3,9 +3,10 @@ public class BankTester{
     BankAccount account1 = new BankAccount(10, 20, "test");
     System.out.println(account1.getBal());
     System.out.println(account1.getID());
-    System.out.println(account1.getPass());
     account1.setPass("test again");
-    System.out.println(account1.getPass());
     System.out.println(account1.deposit(5));
+    System.out.println(account1.withdraw(13));
+    System.out.println(account1.getBal());
+    System.out.println(account1.toString());
   }
 }
