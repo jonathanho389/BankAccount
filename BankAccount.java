87c3a6e79cc1a788592870c98daa8c40b97006f6
Jonathan Ho <hojon389@gmail.com>
2020-10-29 13:25:10 -0400

    changed it so balance is default 0. I misunderstood the problem and thought if there was no parameter it would be 0

--- BankAccount.java
+++ BankAccount.java
@@ -2,18 +2,15 @@ public class BankAccount{
   private double balance;
   private int accountID;
   private String password;
-  public BankAccount(double bal, int id, String pass){
-    balance = bal;
-    bal = 0;
+  public BankAccount(int id, String pass){
+    balance = 0;
     accountID = id;
     password = pass;
   }
   public double getBal(){
-    System.out.print("Your balance is ");
     return balance;
   }
-  public int getID(){
-    System.out.print("Your ID is ");
+  public int getAccountID(){
     return accountID;
   }
   public void setPass(String password1){
@@ -22,30 +19,28 @@ public class BankAccount{
   public boolean deposit(double amount){
     if(amount >= 0){
       balance += amount;
-      System.out.print("Deposit successful: ");
       return true;
     }
     else{
-      System.out.print("You can't deposite negative dollars: ");
       return false;
     }
   }
   public boolean withdraw(double amount){
     if(amount < 0){
-      System.out.print("You can't withdraw negative dollars: ");
       return false;
     }
     else if(balance < amount){
-      System.out.println("You're too broke: ");
       return false;
     }
     else{
       balance -= amount;
-      System.out.print("Withdrawal successful. You have funds: ");
       return true;
     }
   }
+  public boolean authenticate(String password1){
+    return password.equals(password1);
+  }
   public String toString(){
-    return "ID:" + accountID + " "+ "Balance:" + balance;
+    return "#" + accountID + " "+ "$" + balance;
   }
 }
--- BankTester.java
+++ BankTester.java
@@ -1,12 +1,13 @@
 public class BankTester{
   public static void main(String[] args) {
-    BankAccount account1 = new BankAccount(21, 21, "21");
+    BankAccount account1 = new BankAccount(21, "21");
     System.out.println(account1.getBal());
-    System.out.println(account1.getID());
+    System.out.println(account1.getAccountID());
     account1.setPass("test again");
     System.out.println(account1.deposit(5));
     System.out.println(account1.withdraw(5));
     System.out.println(account1.getBal());
     System.out.println(account1.toString());
+    System.out.println(account1.authenticate("test"));
   }
 }
